891568def637d1c7ca97c80fba16dedf7457eff2
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-08-01 19:33:09 +0300

    1

--- cicle-for.js
+++ cicle-for.js
@@ -1,5 +1,15 @@
-for (let i = 10; i <= 10; i -= ) {
-    console.log(i);
-}
+// for (let i = 0; i <= 10; i += 1) {
+//     console.log(i);
+// }
 
-console.log('cicle_is_finished');
+// console.log('cicle_is_finished');
+
+
+
+// Обратный цикл
+
+// for (let i = 10; i >= 0; i -= 1) {
+//     console.log(i);
+// }
+
+// console.log('cicle_is_finished');
