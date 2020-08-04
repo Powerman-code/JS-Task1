749f951d9fde349193b3e5d3caf4342c677abb6d
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-08-04 03:17:32 +0300

    1

--- task2/task5.js
+++ task2/task5.js
@@ -5,3 +5,40 @@
 // Функция проверяет ее на содержание слов spam и sale.
 // Если нашли запрещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.
 // Слова в строке могут быть в произвольном регистре.
+
+function checkForSpam (str) {
+    let message = str.toLowerCase().split(' ');
+    console.log(message);
+    // const searchedWords = ['spam', 'sale'];
+    // console.log(searchedWords);
+
+//    for (mess of message) {
+//        mess
+//    }
+
+//    (message.includes(searchedWords)); { }
+    // console.log(message);
+
+    return (message === 'spam' || message === 'sale');
+
+    // // return message;
+    console.log(message);
+    }
+    // console.log(message.includes('spam' || 'sale'));
+    // console.log(message.includes('sale'));
+
+
+
+
+  console.log(checkForSpam('Latest technology news')); // false
+
+  console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+  console.log(checkForSpam('Get best sale offers now!')); // true
+
+  console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+//   1. Создать переменную
+//   2. Записать туда строку
+//   3. Разбить строку на масив слов
+//   4. Сверить, есть ли искомые слова в масиве.
