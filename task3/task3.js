563804624927dd2404f76907d069a6c9549f05d8
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-08-09 22:37:26 +0300

    1

--- task3/task3.js
+++ task3/task3.js
@@ -7,26 +7,13 @@
 
 const findBestEmployee = function (employees) {
     'use strict';
-    // Write code under this line
-    const values = Object.values(employees);
-    // console.log(values);
 
-    let bestEmployee = values[0];
-    // console.log(bestEmployee);
-
-    for(const value of values) {
-    // console.log(value);
-
-    // console.log(bestEmployee);
+    const entries = Object.entries(employees);
+    console.log(entries);
 
 
-    if (value > bestEmployee) {
-      bestEmployee = value;
-    }
-    console.log(bestEmployee);
-    // return bestEmployee;
-    }
-  };
+
+}
 
   // Объекты и ожидаемый результат
   const developers = {
@@ -38,19 +25,80 @@ const findBestEmployee = function (employees) {
   console.log(findBestEmployee(developers));
   // 'lorence'
 
-  // const supports = {
-  //   poly: 12,
-  //   mango: 17,
-  //   ajax: 4,
-  // };
-  // console.log(findBestEmployee(supports));
+  const supports = {
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  };
+  console.log(findBestEmployee(supports));
   // 'mango'
 
-  // const sellers = {
-  //   lux: 147,
-  //   david: 21,
-  //   kiwi: 19,
-  //   chelsy: 38,
-  // }
-  // console.log(findBestEmployee(sellers));
+  const sellers = {
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }
+  console.log(findBestEmployee(sellers));
   // 'lux'
+
+
+  // --------------------------Вариант 1
+  // const findBestEmployee = function (employees) {
+  //   'use strict';
+
+  //   const values = Object.values(employees);
+
+  //   const keys = Object.keys(employees);
+
+  //   let bestEmployee = '';
+
+  //   let bestEmployeeValue = Math.max(...values);
+  //   // console.log(bestEmployeeValue);
+
+
+  //   for (const key of keys) {
+  //     // console.log(key);
+  //     // console.log(employees[key]);
+
+  //     if (employees[key] === bestEmployeeValue) {
+  //       // console.log('Нашли');
+  //       bestEmployee = key;
+  //       // console.log(bestEmployee);
+
+  //     }
+  //   }
+
+  // return bestEmployee;
+
+
+
+  // ----------------------------------Вариант2
+
+  // const values = Object.values(employees);
+  // // console.log(values);
+
+  // const keys = Object.keys(employees);
+
+  // let bestEmployeeValue = values[0];
+  // // console.log(bestEmployeeValue);
+
+  // let bestEmployee = '';
+
+  // for (const value of values) {
+  //   // console.log(values);
+  //   if (value > bestEmployeeValue) {
+  //     bestEmployeeValue = value;
+  //     // console.log(bestEmployeeValue);
+  //   }
+  // }
+  // // return bestEmployeeValue;
+
+  // for(const key of keys) {
+
+  //   if(employees[key] === bestEmployeeValue) {
+  //     bestEmployee = key;
+  //     // console.log(bestEmployee);
+  //   }
+  // }
+  // return bestEmployee;
