573887cc04ebeaa4ada1f5a4b4a89a02842e9ab8
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-08-14 22:10:49 +0300

    1

---
+++ task4/task1.js
@@ -0,0 +1,44 @@
+// Задача 4-1
+// Callback функция
+// 1) Функция mapArray(array, cb), принимает 1-м параметром array - массив чисел, а вторым параметром cb -
+// функцию обратного вызова (callback).
+
+// 2) Функция mapArray создает новый массив numbers и заполняет его числами
+// из массива array преобразованными функцией cb.
+
+// 3) Оформи создание массива numbers нужной длины используя new Array () и необходимый аргумент для задания длины,
+// равной длине array.
+
+// 4) Напиши функцию обратного вызова addIndex , которая принимает два параметра - element и index и возвращает число
+// - сумму element и index (сложение).
+
+// 5) Напиши функцию обратного вызова subIndex , которая принимает два параметра - element и index и возвращает число
+// - разность element и index (вычитание).
+
+
+// Write code under this line
+const addIndex = (element, index) => element + index;
+// Write code under this line
+const subIndex = (element, index) => element - index;
+
+function mapArray(array, cb) {
+  'use strict';
+// Write code under this line
+  const numbers = new Array(array.length);
+  for(let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+     numbers[i] = cb(element, index);
+  }
+  return numbers;
+}
+
+const arr  = [1,2,3,4,5];
+
+//console.log(mapArray(arr, addIndex));
+// [1, 3, 5, 7, 9]
+
+//console.log(mapArray(arr, subIndex));
+// [1, 1, 1, 1, 1]
+
+
